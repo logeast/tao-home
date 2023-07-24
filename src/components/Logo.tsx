71c0f8e1923cdcf688479193da9fd5ce8b9368c8
logeast <logeast@outlook.com>
2023-07-24 23:11:07 +0800

    feat: 添加 logo 组件

---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+export function Logo() {
+  return (
+    <a href="./" className="w-12 h-12">
+      <img src="/logo.png" alt="桃李" className="w-full h-full" />
+    </a>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import { Logo } from "./Logo";
+
 export function Navbar() {
   return (
     <nav className="w-full bg-amber-50">
       <div className="max-w-screen-xl mx-auto px-8">
         <div className="h-16 flex items-center justify-between gap-4">
           <div className="flex items-center gap-8">
-            <div>logo</div>
+            <Logo />
             <div>菜单</div>
           </div>
           <div className="flex items-center gap-4">
