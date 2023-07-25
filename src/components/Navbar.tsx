b6332352665c8d34011fcb68a487faa4dfdf88fc
logeast <logeast@outlook.com>
2023-07-25 20:12:03 +0800

    feat: 点击展开和隐藏菜单

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { Button } from "./Button";
+import { IconClose } from "./IconClose";
+import { IconMenu } from "./IconMenu";
 import { Logo } from "./Logo";
 import NavMenu, { NavMenuProps } from "./NavMenu";
+import cx from "classnames";
 
 const NAV_MENU_DATA: NavMenuProps["data"] = [
   { key: "1", url: "#home", name: "首页" },
@@ -11,6 +15,7 @@ const NAV_MENU_DATA: NavMenuProps["data"] = [
 ];
 
 export function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
     <nav className="w-full">
       <div className="max-w-screen-xl mx-auto px-8 max-md:hidden">
@@ -28,8 +33,19 @@ export function Navbar() {
       <div className="max-w-screen-xl mx-auto px-8 max-md:visible md:hidden">
         <div className="h-16 flex items-center justify-between gap-4">
           <Logo />
+          <button onClick={() => setOpen(!open)}>
+            {open ? (
+              <IconClose className="w-6 h-6" />
+            ) : (
+              <IconMenu className="w-6 h-6" />
+            )}
+          </button>
         </div>
-        <div>
+        <div
+          className={cx({
+            hidden: !open,
+          })}
+        >
           <NavMenu data={NAV_MENU_DATA} />
           <div className="py-4 flex flex-col gap-4">
             <Button>登录</Button>
